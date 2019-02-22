bec9d35bac23d8925de3953b0ee275bb9f9f85f9
Mogens Heller Grabe <mookid8000@gmail.com>
2019-02-22 12:05:41 +0100

    try some stuff out

---
+++ KafkaTjek.Tests/TestKafkaProducer.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using Testy;
+
+namespace KafkaTjek.Tests
+{
+    [TestFixture]
+    public class TestKafkaProducer : FixtureBase
+    {
+        KafkaProducer _producer;
+
+        protected override void SetUp()
+        {
+            _producer = new KafkaProducer("localhost:9092");
+
+            Using(_producer);
+        }
+
+        [Test]
+        public async Task CanSendEvents()
+        {
+            await _producer.SendAsync("test-topic",
+                new[]
+                {
+                    new KafkaEvent("key1", "hej"),
+                    new KafkaEvent("key2", "med"),
+                    new KafkaEvent("key2", "dig")
+                });
+        }
+    }
+}
---
+++ KafkaTjek/KafkaProducer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaTjek
+{
+    public class KafkaProducer : IDisposable
+    {
+        static readonly Headers EmptyHeaders = new Headers();
+
+        readonly Producer<string, string> _producer;
+
+        public KafkaProducer(string address)
+        {
+            var config = new ProducerConfig { BootstrapServers = address};
+            
+            _producer = new ProducerBuilder<string,string>(config)
+                .Build();
+        }
+
+        public Task SendAsync(string topic, IEnumerable<KafkaEvent> events)
+        {
+            var taskCompletionSource = new TaskCompletionSource<object>();
+
+            foreach (var evt in events)
+            {
+                var message = new Message<string, string>
+                {
+                    Key = evt.Key,
+                    Headers = GetHeaders(evt.Headers),
+                    Value = evt.Body
+                };
+                _producer.BeginProduce(topic, message);
+            }
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                _producer.Flush(CancellationToken.None);
+                taskCompletionSource.SetResult(null);
+            });
+
+            return taskCompletionSource.Task;
+        }
+
+        static Headers GetHeaders(Dictionary<string, string> dictionary)
+        {
+            if (dictionary.Count == 0) return EmptyHeaders;
+
+            var headers = new Headers();
+            
+            foreach (var (key, value) in dictionary)
+            {
+                headers.Add(key, Encoding.UTF8.GetBytes(value));
+            }
+            
+            return headers;
+        }
+
+        public void Dispose() => _producer.Dispose();
+    }
+}
